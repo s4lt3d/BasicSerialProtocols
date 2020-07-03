7b1b0d7385ed63b7e9ad9bc7fd967280bd131702
Walter <walter.gordy@gmail.com>
2020-07-03 17:29:00 -0600

    Update FourienProtocolTest.ino

--- FourienProtocolTest.ino
+++ FourienProtocolTest.ino
@@ -8,23 +8,29 @@ CircularBuffer<char, 255> buffer;
 
 #define DEBUG 1
 
+#define VERSION                "Ver 0.011 2020 07 03"
+#define PREAMBLE               0x55
+#define PREAMBLE_COUNT         4   // must recieve 4 preambles in a row to start a packet
+#define DIGITAL_IO_MESSAGE     0x0E
+#define ASCII_MESSAGE          'A'
+#define VERSION_MESSAGE        85
+#define MEMORY_READ_MESSAGE    0x10
+#define MEMORY_WRITE_MESSAGE   0x20
 
-#define PREAMBLE            0x55
-#define PREAMBLE_COUNT      4   // must recieve 4 preambles in a row to start a packet
-#define DIGITAL_IO_MESSAGE  0x0E
-#define ASCII_MESSAGE       'A'
+
+unsigned long register_memory[255]; // 
 
 void setup() {
   // Run usb serial at 8mb/s 
-  Serial.begin(8000000);
-
+  Serial.begin(2000000);
+  sendAsciiMessage(VERSION_MESSAGE);
   pinMode(13, OUTPUT);
   digitalWrite(13, 1);
   
 }
 
 int packet_flag = 0;
-byte data = 0;
+
 int incoming_byte;
 int expected_size = 2;
 int preamble_count = 0;
@@ -59,11 +65,17 @@ void serviceIncomingSerial() {
 }
 
 void parseBuffer() {
-  sendAsciiMessage("Parsing Buffer");
+  debug("Parsing Buffer");
   char len = buffer.shift();
   char type = buffer.shift();
+
+  debug(String(len, DEC));
+  debug(String(type, DEC));
   char pin;
   char val;
+
+  unsigned int address = 0;
+  unsigned long   data = 0;
   
   switch(type) {
     case DIGITAL_IO_MESSAGE:
@@ -78,24 +90,47 @@ void parseBuffer() {
         digitalWrite(pin, val);
       break; // DIGITAL_IO_MESSAGE
 
-      
-    case '1':
-      sendAsciiMessage("Packet 1");
-      pin = buffer.shift();
-      val = buffer.shift();
+    case VERSION_MESSAGE:
+      sendAsciiMessage(VERSION);
+      break;
 
-      if(pin == '2'){
-        sendAsciiMessage("Pin 2");
-        if(val == '0'){
-          sendAsciiMessage("Off");
-          digitalWrite(13, 0);
-        }
-        if(val == '1'){
-          sendAsciiMessage("On");
-          digitalWrite(13, 1);
-        }
-      break; // case '1'
-    }
+    case MEMORY_WRITE_MESSAGE:
+      address = ((unsigned int)buffer.shift()) << 8;
+      address |= ((unsigned int)buffer.shift());
+
+      address &= 0xFFFF;
+
+      data  = ((unsigned long)buffer.shift()) << 24;
+      data |= ((unsigned long)buffer.shift()) << 16;
+      data |= ((unsigned long)buffer.shift()) << 8;
+      data |= (unsigned long)buffer.shift();
+      register_memory[address] = data;
+      data = register_memory[address];
+      debug("Set Register");
+      debug(String(address, DEC));
+      debug(String(data, DEC));
+      break;
+
+    case MEMORY_READ_MESSAGE:
+      debug("Read memory");
+      address = (unsigned int)buffer.shift() << 8;
+      address |= (unsigned int)buffer.shift();
+      address &= 0xFFFF;
+      data = register_memory[address];
+      sendPreamble();
+
+      Serial.write(7);
+      Serial.write(MEMORY_READ_MESSAGE);
+      Serial.write((byte)((address >> 8) & 0xFF));
+      Serial.write((byte)((address) & 0xFF));
+      Serial.write((byte)((data >> 24) & 0xFF));
+      Serial.write((byte)((data >> 16) & 0xFF));
+      Serial.write((byte)((data >> 8) & 0xFF));
+      Serial.write((byte)((data) & 0xFF));
+      
+      debug(String(address, DEC));
+      debug(String(data, DEC));
+      break;
   }
   
   buffer.clear();
@@ -118,20 +153,3 @@ void sendAsciiMessage(String message) {
   Serial.write(ASCII_MESSAGE);
   Serial.println(message);
 } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
