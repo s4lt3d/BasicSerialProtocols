d7498eadc52684fd425ebd9e8e2cd8248cc8d259
Walter <walter.gordy@gmail.com>
2020-07-22 12:18:29 -0600

    Update FourienProtocol.cs

--- FourienProtocol.cs
+++ FourienProtocol.cs
@@ -7,8 +7,9 @@ using System.IO.Ports;
 
 namespace Fourien
 {
-    public delegate void NotifyAsciiMessage(string message);
-    public delegate void NotifyRegisterReadMessage(int reg, long val);
+    public delegate void NotifyAsciiMessage(string message, long time);
+    public delegate void NotifyRegisterReadMessage(int reg, long[] memory, int len, long time);
+    public delegate void NotifyAnalogReadMessage(int pin, long val, long time);
 
     class FourienProtocol
     {
@@ -19,17 +20,22 @@ namespace Fourien
 
         const int PREAMBLE = 0x55;
         const int PREAMBLE_COUNT = 4;
-        const int DIGITAL_IO_MESSAGE = 0x0E;
+        const int DIGITAL_WRITE_MESSAGE = 0x0E;
+        const int DIGITAL_READ_MESSAGE = 0x1E;
+        const int ANALOG_WRITE_MESSAGE = 0x2E;
+        const int ANALOG_READ_MESSAGE = 0x3E;
         const int ASCII_MESSAGE = 65; // 'A'
         const int VERSION_MESSAGE = 85;
         const int MEMORY_READ_MESSAGE = 0x10;
         const int MEMORY_WRITE_MESSAGE = 0x20;
+        const int MEMORY_BURST_READ_MESSAGE = 0x11;
+        const int MEMORY_BURST_WRITE_MESSAGE = 0x21;
 
         public event NotifyAsciiMessage AsciiMessageRecieved;
         public event NotifyRegisterReadMessage RegisterReadMessage;
+        public event NotifyAnalogReadMessage RegisterAnalogReadMessage;
 
-
-        public FourienProtocol(string portName, int baud = 2000000)
+        public FourienProtocol(string portName, int baud = 1600)
         {
 
             serial = new SerialPort(portName, baud);
@@ -39,23 +45,29 @@ namespace Fourien
 
         public void Open()
         {
+            
             serial.Open();
+            // flush incoming bytes, yes this is an issue. 
+            while (serial.BytesToRead > 0) 
+                serial.ReadByte();
         }
 
 
         int preamble_count = 0;
-        int buf_pos = 0;
 
         private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             while (serial.BytesToRead > 0)
             {
+                
                 int incoming_byte = serial.ReadByte();
+                
                 buffer.Enqueue((byte)incoming_byte);
 
                 if (incoming_byte == PREAMBLE)
                 {
                     preamble_count++;
+                 //   Console.WriteLine();
                 }
                 else
                 {
@@ -64,19 +76,23 @@ namespace Fourien
                         //while (buffer.Peek() == PREAMBLE)
                         //    _ = buffer.Dequeue();
                         buffer.Clear();
+                        
                         buffer.Enqueue((byte)incoming_byte);
                     }
 
                     preamble_count = 0;
 
-                    if (buffer.Count > 0)
+                    if (buffer.Count > 1)
                     {
-                        if (buffer.Count == buffer.Peek() + 1)
+                        int size = (buffer.ElementAt(0) << 8) + buffer.ElementAt(1);
+
+                        if (buffer.Count == size + 1)
                         {
                             parseBuffer();
                         }
                     }
                 }
+                //Console.Write(incoming_byte.ToString("X2") + " ");
             }
         }
 
@@ -99,26 +115,28 @@ namespace Fourien
         /// <param name="value">0 or 1</param>
         public void DigitalWrite(int pin, int val)
         {
-            byte[] data = new byte[9];
+            byte[] data = new byte[16];
             int i = 0;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
+            data[i++] = 0;
             data[i++] = 4; // size of packet after this. Max size 250 bytes
-            data[i++] = DIGITAL_IO_MESSAGE;
+            data[i++] = DIGITAL_WRITE_MESSAGE;
             data[i++] = (byte)pin;
             data[i++] = (byte)val;
             SendData(data, i);
         }
 
         public void FirmwareVersion() {
-            byte[] data = new byte[8];
+            byte[] data = new byte[16];
             int i = 0;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
+            data[i++] = 0;
             data[i++] = 1; // size of packet after this. Max size 250 bytes
             data[i++] = VERSION_MESSAGE;
             SendData(data, i);
@@ -134,6 +152,7 @@ namespace Fourien
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
+            data[i++] = 0; // length leading zero
             data[i++] = 8; // length including this byte
             data[i++] = MEMORY_WRITE_MESSAGE;
             data[i++] = (byte)((reg >> 8) & 0xFF);
@@ -145,6 +164,42 @@ namespace Fourien
             SendData(data, i);
         }
 
+        public void AnalogWrite(int pin, long val)
+        {
+            byte[] data = new byte[16];
+            int i = 0;
+            pin &= 0xFF;
+
+            data[i++] = PREAMBLE;
+            data[i++] = PREAMBLE;
+            data[i++] = PREAMBLE;
+            data[i++] = PREAMBLE;
+            data[i++] = 0; // length leading zero
+            data[i++] = 5; // length including this byte
+            data[i++] = ANALOG_WRITE_MESSAGE;
+            data[i++] = (byte)((pin) & 0xFF);
+            data[i++] = (byte)((val >> 8) & 0xFF);
+            data[i++] = (byte)((val) & 0xFF);
+            SendData(data, i);
+        }
+
+        public void AnalogRead(int pin)
+        {
+            byte[] data = new byte[16];
+            int i = 0;
+            pin &= 0xFF;
+
+            data[i++] = PREAMBLE;
+            data[i++] = PREAMBLE;
+            data[i++] = PREAMBLE;
+            data[i++] = PREAMBLE;
+            data[i++] = 0; // length leading zero
+            data[i++] = 3; // length including this byte
+            data[i++] = ANALOG_READ_MESSAGE;
+            data[i++] = (byte)((pin) & 0xFF);
+            SendData(data, i);
+        }
+
         public void RegisterRead(int reg) {
             byte[] data = new byte[16];
             int i = 0;
@@ -153,7 +208,8 @@ namespace Fourien
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
             data[i++] = PREAMBLE;
-            data[i++] = 4; // length including this byte
+            data[i++] = 0; // length leading zero
+            data[i++] = 5; // length including this byte
             data[i++] = MEMORY_READ_MESSAGE;
             data[i++] = (byte)((reg >> 8) & 0xFF);
             data[i++] = (byte)((reg) & 0xFF);
@@ -162,36 +218,84 @@ namespace Fourien
 
         private void parseBuffer()
         {
-            int len = buffer.Dequeue();
+            long time = 0;
+            if (buffer.Count < 3)
+                return;
+            int len = buffer.Dequeue() << 8;
+            len |= buffer.Dequeue();
 
             int type = buffer.Dequeue();
 
 
             switch (type)
             {
-                case DIGITAL_IO_MESSAGE:
+                case DIGITAL_WRITE_MESSAGE:
+
+                    break;
+
+                case DIGITAL_READ_MESSAGE:
+
+                    break;
+
+                case ANALOG_WRITE_MESSAGE:
 
                     break;
 
                 case ASCII_MESSAGE:
                     string message = "";
-                    while(buffer.Count > 0)
+                    time |= (long)buffer.Dequeue() << 24;
+                    time |= (long)buffer.Dequeue() << 16;
+                    time |= (long)buffer.Dequeue() << 8;
+                    time |= (long)buffer.Dequeue();
+
+                    while (buffer.Count > 0)
                         message += (char)buffer.Dequeue();
-                    message += "\r\n";
-                    OnAsciiMessageRecieved(message);
+                    
+                    OnAsciiMessageRecieved(message, time);
                     break;
 
                 case MEMORY_READ_MESSAGE:
 
+                    time |= (long)buffer.Dequeue() << 24;
+                    time |= (long)buffer.Dequeue() << 16;
+                    time |= (long)buffer.Dequeue() << 8;
+                    time |= (long)buffer.Dequeue();
+
                     int address = 0;
                     address |= (int)buffer.Dequeue() << 8;
                     address |= (int)buffer.Dequeue();
+
+                    long[] memory = new long[2048];
+                    int i = 8;
+                    int pos = 0;
                     long data = 0;
-                    data |= (long)buffer.Dequeue() << 24;
-                    data |= (long)buffer.Dequeue() << 16;
-                    data |= (long)buffer.Dequeue() << 8;
-                    data |= (long)buffer.Dequeue();
-                    OnRegisterReadMessage(address, data);
+                    while (i < len)
+                    {
+                        data = (long)buffer.Dequeue() << 24;
+                        data |= (long)buffer.Dequeue() << 16;
+                        data |= (long)buffer.Dequeue() << 8;
+                        data |= (long)buffer.Dequeue();
+                        memory[pos] = data;
+                        i += 4;
+                        pos++;
+                    }
+                    
+                    OnRegisterReadMessage(address, memory, pos, time);
+                    break;
+                case ANALOG_READ_MESSAGE:
+
+                    time |= (long)buffer.Dequeue() << 24;
+                    time |= (long)buffer.Dequeue() << 16;
+                    time |= (long)buffer.Dequeue() << 8;
+                    time |= (long)buffer.Dequeue();
+
+                    int pin = 0;
+                    pin = (int)buffer.Dequeue();
+                    long value = 0;
+                    value = (long)buffer.Dequeue() << 8;
+                    value |= (long)buffer.Dequeue();
+                    
+                    OnAnalogReadMessage(pin, value, time);
                     break;
 
                 default:
@@ -204,16 +308,30 @@ namespace Fourien
                     _ = buffer.Dequeue();
         }
 
-        protected virtual void OnAsciiMessageRecieved(string message) //protected virtual method
+        protected virtual void OnAsciiMessageRecieved(string message, long time) //protected virtual method
         {
             //if AsciiMessageRecieved is not null then call delegate
-            AsciiMessageRecieved?.Invoke(message);
+            AsciiMessageRecieved?.Invoke(message, time);
         }
 
-        protected virtual void OnRegisterReadMessage(int reg, long val) //protected virtual method
+        protected virtual void OnRegisterReadMessage(int reg, long[] memory, int len, long time) //protected virtual method
         {
             //if AsciiMessageRecieved is not null then call delegate
-            RegisterReadMessage?.Invoke(reg, val);
+            RegisterReadMessage?.Invoke(reg, memory, len, time);
+        }
+
+        protected virtual void OnAnalogReadMessage(int pin, long val, long time) //protected virtual method
+        {
+            //if AsciiMessageRecieved is not null then call delegate
+            RegisterAnalogReadMessage?.Invoke(pin, val, time);
+        }
+
+        public void Close() {
+            try
+            {
+                serial.Close();
+            }
+            catch { }
         }
     }
 }
